4c04086ed1d8a3155b45897ca44f3333da7eeca5
Abdullah Al-Rashidi <alrashidi@nasaqagency.com>
2025-07-28 19:20:04 +0300

    task 1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Cookies & Beyond</Text>
+    <View style={styles.container}>
+      <Text style={styles.H1}>Cookies & Beyond</Text>
       <Text>Where Cookies maniacs gather</Text>
       <Image
+        style={styles.headerimg}
         source={{
-          uri: "https://kagi.com/proxy/Milk-Mustache-Interior-2-1024x683.jpg?c=uLaS44I7v_cvBBNIaEb9rSqhO2ATSmUoO0S2itwUYE9IdHGmWTbt1amYtu4_obLxayqxJC1xeO3-nY8NZPWrv4Qq8EFRk9FLzFQj2GZuG-TCVw6OJhsB87KZPuJmt5mSVzlT-GnlZGq028lTJnaH4g%3D%3D",
+          uri: "https://kagi.com/proxy/storefront1*1200xx2184-2130-512-262.jpg?c=_oUJLZZEdWvlw_WKDLiWzKXtM8j0WuCDqyJJeUaXCTC863mVQWn0UZyfwn_PA4SPvKPMOh8cRZaSIF26XVdaN9e_o-fQ4a5IS0g30VagPF1uuv7KT3dIxle8z65uaxzv",
         }}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerimg: {
+    width: 500,
+    height: 500,
+  },
+  H1: {
+    fontSize: 50,
+  },
+  container: {
+    padding: 15,
+    alignItems: "center",
+  },
+});
